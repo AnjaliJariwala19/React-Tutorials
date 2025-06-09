20cf5ae5d501c2d75f4170cf9365def2b5b44aa7
AnjaliJariwala19 <anjalijariwala3@gmail.com>
2025-06-09 13:59:10 +0530

    File Uploaded

--- 03_Hooks_Counter/src/App.jsx
+++ 03_Hooks_Counter/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 //React controls the UI. And for the UI updation react uses HOOKS.
@@ -11,33 +9,45 @@ import './App.css'
 
 function App() {
 
-  let [counter,setCounter] = useState(5) //react hook
-  
+  let [counter, setCounter] = useState(5) //react hook
+
   //let counter = 5;
 
-  const adding =()=>{
-    counter = counter + 1
-    setCounter(counter)
-    //console.log(counter)
+  const adding = () => {
+    if (counter >= 20) {
+      alert('Counter cannot exceed 20')
+      counter = 20;
+    }
+    else {
+      counter = counter + 1
+      setCounter(counter)
+      //console.log(counter)
+    }
   }
 
-  const subtracting =() =>{
-    counter = counter - 1
-    setCounter(counter)
-    //console.log(counter)
+  const subtracting = () => {
+    if (counter <= 0) {
+      alert('Counter cannot be negative.')
+      counter = 0
+    }
+    else {
+      counter = counter - 1
+      setCounter(counter)
+      //console.log(counter)
+    }
 
   }
 
   return (
     <>
-    <h1>Anjali's</h1>
-    <h2>Counter : {counter}</h2>
-    <button onClick={adding}>Plus One</button> 
-    <br/>
-    <br/>
-    <button onClick={subtracting}>Minus One</button>
-    <br />
-    <p>Current counter value:{counter}</p>
+      <h1>Anjali's</h1>
+      <h2>Counter : {counter}</h2>
+      <button onClick={adding}>Plus One</button>
+      <br />
+      <br />
+      <button onClick={subtracting}>Minus One</button>
+      <br />
+      <p>Current counter value:{counter}</p>
     </>
   )
 }
